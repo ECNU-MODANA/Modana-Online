e196643d44a40e5e5bf338b31bd4b92fa07c0dca
jiangkaiqiang <jiang997160328@outlook.com>
2016-10-19 12:21:25 +0800

    wls project update

--- wls-web/src/main/webapp/app/js/app.js
+++ wls-web/src/main/webapp/app/js/app.js
@@ -1,5 +1,5 @@
-var coldWeb = angular.module('ColdWeb', ['ui.bootstrap', 'ui.router', 'ui.checkbox','ngCookies', 'xeditable', 'isteven-multi-select', 'angucomplete', 'angular-table','ngFileUpload','remoteValidation', 'jkuri.gallery']);
-if (typeof String.prototype.startsWith != 'function') {  //支持startsWith
+var wls = angular.module('WLS', ['ui.bootstrap', 'ui.router', 'ui.checkbox','ngCookies', 'xeditable', 'isteven-multi-select', 'angucomplete', 'angular-table','ngFileUpload','remoteValidation', 'jkuri.gallery']);
+if (typeof String.prototype.startsWith != 'function') {
     String.prototype.startsWith = function (prefix){  
      return this.slice(0, prefix.length) === prefix;  
     };  
@@ -7,227 +7,30 @@ if (typeof String.prototype.startsWith != 'function') {  //支持startsWith
 angular.element(document).ready(function ($ngCookies, $http, $rootScope) {
 	    $.ajax({type: "GET",cache: false,dataType: 'json',url: '/i/user/findUser'}).success(function(data){
 	    	user = data;
-	    	angular.bootstrap(document, ['ColdWeb']);
+	    	angular.bootstrap(document, ['WLS']);
 	    });
 });
-coldWeb.run(function (editableOptions, naviService,userService) {
+wls.run(function (editableOptions, naviService,userService) {
     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-    naviService.setNAVI();
     userService.setUser(user);
 });
 
-coldWeb.config(function ($httpProvider) {
-    $httpProvider.interceptors.push(function ($q, $injector) {
-        return {
-            'response': function (response) {
-                return response;
-            },
-            'responseError': function (rejection) {
-                var modal = $injector.get('$Modal');
-                modal.open({
-                    animation: true,
-                    templateUrl: 'app/template/error.html',
-                    controller: 'error',
-                    backdrop: true,
-                    resolve: {
-                        rejection: function () {
-                            return rejection;
-                        }
-                    }
-                });
-
-                return $q.reject(rejection);
-            }
-        };
-    });
-});
-
-
-coldWeb.factory('userService',['$rootScope','$http', function($rootScope,$http){
+wls.factory('userService',['$rootScope','$http', function($rootScope,$http){
 	return {
 		setUser: function(user){
 	    	$rootScope.user = user;
 	    	$rootScope.logout = function () {
 	        	$.ajax({type: "GET",cache: false,dataType: 'json',url: '/i/user/logout'}).success(function(data){});
-	        	 $rootScope.user =window.user=user=null;//清除系统user;
-	        	 window.location.href="#/home";
+	        	 $rootScope.user =window.user=user=null;
+	        	 window.location.href="#/index";
 	        };
-	        $rootScope.gotoSmartCold = function(){
-//	        	cookies = document.cookie.split(";");
-	        	url = "http://www.smartcold.org.cn";
-//	        	angular.forEach(cookies,function(item){
-//	        		item = item.trim();
-//	        		if(item.startsWith("token=")){	        			
-//	        			url = url + "/#/" + item;
-//	        		}
-//	        	});
-	        	window.open(url);
-	        }
 	    },
-	}
-}])
-
-coldWeb.factory('naviService', ['$rootScope', '$state', function ($rootScope, $state) {
-    return {
-        setNAVI: function () {
-            $rootScope.toColdStorageList = function () {
-                $state.go('coldStoragelist', {});
-            };
-            $rootScope.toColdStorageComment = function (storageId) {
-                $state.go('coldStorageComment', {'storageId': storageId});
-            };
-            $rootScope.toColdShareComment = function (storageId) {
-            	$state.go('coldShareComment',{});
-            };
-        },
-    };
+	};
 }]);
-
-coldWeb.filter('objectCount', function () {
-    return function (input) {
-        var size = 0, key;
-        for (key in input) {
-            if (input.hasOwnProperty(key)) size++;
-        }
-        return size;
-    }
-});
-
-coldWeb.filter('toArray', function () {
-    'use strict';
-
-    return function (obj) {
-        if (!(obj instanceof Object)) {
-            return obj;
-        }
-
-        return Object.keys(obj).filter(function (key) {
-            if (key.charAt(0) !== "$") {
-                return key;
-            }
-        }).map(function (key) {
-            if (!(obj[key] instanceof Object)) {
-                obj[key] = {value: obj[key]};
-            }
-
-            return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
-        });
-    };
-});
-
-
-coldWeb.directive('snippet', function () {
-    return {
-        restrict: 'E',
-        template: '<pre><div class="hidden code" ng-transclude></div><code></code></pre>',
-        replace: true,
-        transclude: true,
-        link: function (scope, elm, attrs) {
-            scope.$watch(function () {
-                return elm.find('.code').text();
-            }, function (newValue, oldValue) {
-                if (newValue != oldValue) {
-                    elm.find('code').html(hljs.highlightAuto(newValue).value);
-                }
-            });
-        }
-    };
-});
-
-
-coldWeb.directive('star', function () {
-  return {
-    template: '<ul class="rating" ng-mouseleave="leave(order)">' +
-        '<li ng-repeat="star in stars" ng-class="star" ng-click="click(order,$index + 1)" ng-mouseover="over(order,$index + 1)">' +
-        '\u2605' +
-        '</li>' +
-        '</ul>{{overVal?overVal:ratingVal}} 分',
-    scope: {
-      ratingValue: '=',
-      max: '=',
-      order: '=',
-      readonly: '@',
-      onClick: '='
-    },
-    controller: function($scope){
-      $scope.ratingValue = $scope.ratingValue || 0;
-      $scope.max = $scope.max || 5;
-	  $scope.ratingVal = 0;
-	  $scope.overVal = null;
-	  $scope.stars = [];
-	  for(var i=0; i< $scope.max;i++){
-		  $scope.stars.push({
-              filled: i < $scope.ratingVal
-            });
-	  }
-	  var updateStars = function (val) {
-          for (var i = 0; i < $scope.max; i++) {
-            $scope.stars[i].filled = i < val;
-          }
-        };
-      $scope.click = function(i,val){
-        $scope.ratingVal = val;
-    	updateStars(val);
-        $scope.onClick(i,val);
-      };
-      $scope.over = function(i,val){
-    	$scope.overVal = val;
-    	updateStars(val);
-      };
-      $scope.leave = function(i){
-    	  $scope.overVal = null;
-    	updateStars($scope.ratingVal);
-      }
-      updateStars(0);
-    },
-    link: function (scope, elem, attrs) {
-      elem.css("text-align", "center");
- 
-    }
-  };
-});
-
-coldWeb.directive('activeLink', ['$location', '$filter', function (location, filter) {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs, controller) {
-            var clazz = attrs.activeLink;
-            var path = element.children().attr('href') + "";
-            path = filter('limitTo')(path, path.length - 1, 1);
-            scope.location = location;
-            scope.$watch('location.path()', function (newPath) {
-                if (newPath.indexOf(path) > -1) {
-                    element.addClass(clazz);
-                } else {
-                    element.removeClass(clazz);
-                }
-            });
-        }
-    };
-}]);
-
-coldWeb.filter('sizeformat', function () {
-    return function (size) {
-        if (size / (1024 * 1024 * 1024) > 1)
-            return (size / (1024 * 1024 * 1024)).toFixed(2) + 'G';
-        else if (size / (1024 * 1024) > 1)
-            return (size / (1024 * 1024)).toFixed(2) + 'M';
-        else if (size / 1024 > 1)
-            return (size / 1024).toFixed(2) + 'K';
-        else
-            return size + 'B'
-    }
-});
-
 
 JS.Engine.start('conn');
 JS.Engine.on(
         { 
-          /* // 对应服务端 “频道1” 的值 msgCount
-           msgCount : function(msgCount){
-               alert(msgCount);
-           },*/
-           // 对应服务端 “频道2” 的值 msgData
            msgData : function(msgData){
         	   //alert(msgData);
         	   $(".msgPush").fadeIn();
@@ -237,8 +40,7 @@ JS.Engine.on(
        }
    );
 
-
-coldWeb.config(function ($stateProvider, $urlRouterProvider) {
+wls.config(function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/home");
     //index
     $stateProvider.state('login', {
@@ -273,27 +75,27 @@ coldWeb.config(function ($stateProvider, $urlRouterProvider) {
         url: '/coldStorageComment/:rdcID',
         controller: 'coldStorageComment',
         templateUrl: 'app/template/coldStorageComment.html'
-    }).state('coldShareComment', {  //共享257
+    }).state('coldShareComment', {  
         url: '/coldShareComment',
         controller: 'coldShareComment',
-        params:{_cuttid:null,showData:null,codeCode:null},//
+        params:{_cuttid:null,showData:null,codeCode:null},
         templateUrl: 'app/template/coldsharerdc.html'
-    }).state('releaseItem', {  //免费发布商品-货品-车
+    }).state('releaseItem', { 
         url: '/releaseItem',
         controller: 'releaseItem',
         params:{data:null,dataid:null,_cuttid:null},
         templateUrl: 'app/template/release_item.html'
-    }).state('releaseItemList', {  //免费发布商品-货品-车-list
+    }).state('releaseItemList', { 
         url: '/releaseItemList',
         controller: 'releaseItemList',
         params:{data:null,dataid:null,_cuttid:null},
         templateUrl: 'app/template/release_item_list.html'
-    }).state('releaseCarInfo', {  //免费发布车信息
+    }).state('releaseCarInfo', {  
 		url: '/releaseCarInfo',
 		controller: 'releaseCarInfo',
 		params:{data:null,dataid:null,_cuttid:null},
 		templateUrl: 'app/template/release_carinfo.html'
-    }).state('shareriteminfo', {  //共享详情
+    }).state('shareriteminfo', {  
 		url: '/shareriteminfo',
 		controller: 'shareriteminfo',
 		params:{dataid:null},
