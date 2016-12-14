dabbd81f425da0066b7a6800bd49a4bdd5cb1f67
jiangkaiqiang <jiang997160328@outlook.com>
2016-12-14 20:59:03 +0800

    update new

--- wls-web/src/main/java/com/wls/manage/controller/InformationController.java
+++ wls-web/src/main/java/com/wls/manage/controller/InformationController.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
